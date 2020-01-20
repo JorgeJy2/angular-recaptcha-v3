71fcbdd1a465e5463f4ff823f4e4f3eb0c79b54c
Jorge Jacobo <jorge.jacobo.francisco.306@gmail.com>
2020-01-19 23:03:34 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import {  ReCaptchaV3Service } from 'ng-recaptcha';
+import { FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recaptchatres';
+
+  constructor(
+    private recaptchaV3Service: ReCaptchaV3Service,
+  ) {
+  }
+ 
+  public executeImportantAction(): void {
+    this.recaptchaV3Service.execute('importantAction').subscribe(console.log);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha';
+ 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecaptchaV3Module
   ],
-  providers: [],
+  providers: [
+    { provide: RECAPTCHA_V3_SITE_KEY, useValue: '6LdiqNAUAAAAAEFdJAgqdOA1AwImGW-HJ1kRL_l2' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
